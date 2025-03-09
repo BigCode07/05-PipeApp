e923e3dba888a5f4c108e903f0b57b4e5734cf6c
bigcode <bigcode07@gmail.com>
2025-03-09 20:01:45 -0300

    Se termino el curso basico de Pipes

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import {
+  AsyncPipe,
+  I18nPluralPipe,
+  I18nSelectPipe,
+  JsonPipe,
+  KeyValuePipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { interval, map, tap } from 'rxjs';
 
 const client1 = {
   name: 'Nicolas',
@@ -18,7 +28,17 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [
+    CardComponent,
+    I18nSelectPipe,
+    I18nPluralPipe,
+    SlicePipe,
+    JsonPipe,
+    UpperCasePipe,
+    KeyValuePipe,
+    TitleCasePipe,
+    AsyncPipe,
+  ],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -38,4 +58,38 @@ export default class UncommonPageComponent {
 
     this.client.set(client1);
   }
+
+  //i18n Plural
+  clientsMap = signal({
+    '=0': 'No tenemos ningun cliente esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  });
+
+  clients = signal(['Maria', 'Pedro', 'Fernando', 'Juan', 'Carlos']);
+
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1));
+  }
+
+  profile = {
+    name: 'Nicolas',
+    age: 26,
+    address: 'Cordoba, Argentina',
+  };
+
+  //Async Pipe
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // reject('Tenemos un error en la data');
+      // resolve('Tenemos data en la promesa');
+      // console.log('Promesa finalizada');
+    }, 3500);
+  });
+
+  myObservableTimer = interval(2000).pipe(
+    map((value) => value + 1),
+    tap((value) => console.log('tap:', value))
+  );
 }
